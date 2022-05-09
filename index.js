c239871ed9efb5a6a3a693a603519a70a3ece0c6
Anton Novikov <3483778@gmail.com>
2022-05-09 16:38:16 +0300

    feat: virtual keyboard finished

---
+++ index.js
@@ -0,0 +1,790 @@
+const keyboard = {
+  0: {
+    code: 'Backquote',
+    keyCode: 192,
+    en: '`',
+    enShift: '˜',
+    ru: 'ё',
+    ruShift: 'Ë',
+  },
+  1: {
+    code: 'Digit1',
+    keyCode: 49,
+    en: '1',
+    enShift: '!',
+    ru: '1',
+    ruShift: '!',
+  },
+  2: {
+    code: 'Digit2',
+    keyCode: 50,
+    en: '2',
+    enShift: '@',
+    ru: '2',
+    ruShift: '"',
+  },
+  3: {
+    code: 'Digit3',
+    keyCode: 51,
+    en: '3',
+    enShift: '#',
+    ru: '3',
+    ruShift: '№',
+  },
+  4: {
+    code: 'Digit4',
+    keyCode: 52,
+    en: '4',
+    enShift: '$',
+    ru: '4',
+    ruShift: ';',
+  },
+  5: {
+    code: 'Digit5',
+    keyCode: 53,
+    en: '5',
+    enShift: '%',
+    ru: '5',
+    ruShift: '%',
+  },
+  6: {
+    code: 'Digit6',
+    keyCode: 54,
+    en: '6',
+    enShift: 'ˆ',
+    ru: '6',
+    ruShift: ':',
+  },
+  7: {
+    code: 'Digit7',
+    keyCode: 55,
+    en: '7',
+    enShift: '＆',
+    ru: '7',
+    ruShift: '?',
+  },
+  8: {
+    code: 'Digit8',
+    keyCode: 56,
+    en: '8',
+    enShift: '*',
+    ru: '8',
+    ruShift: '*',
+  },
+  9: {
+    code: 'Digit9',
+    keyCode: 57,
+    en: '9',
+    enShift: '(',
+    ru: '9',
+    ruShift: '(',
+  },
+  10: {
+    code: 'Digit0',
+    keyCode: 48,
+    en: '0',
+    enShift: ')',
+    ru: '0',
+    ruShift: ')',
+  },
+  11: {
+    code: 'Minus',
+    keyCode: 189,
+    en: '-',
+    enShift: '_',
+    ru: '-',
+    ruShift: '_',
+  },
+  12: {
+    code: 'Equal',
+    keyCode: 187,
+    en: '=',
+    enShift: '+',
+    ru: '=',
+    ruShift: '+',
+  },
+  13: {
+    code: 'Backspace',
+    keyCode: 8,
+    en: 'Backspace',
+    enShift: 'Backspace',
+    ru: 'Backspace',
+    ruShift: 'Backspace',
+  },
+  14: {
+    code: 'Tab',
+    keyCode: 9,
+    en: 'Tab',
+    enShift: 'Tab',
+    ru: 'Tab',
+    ruShift: 'Tab',
+  },
+  15: {
+    code: 'KeyQ',
+    keyCode: 81,
+    en: 'q',
+    enShift: 'Q',
+    ru: 'й',
+    ruShift: 'Й',
+  },
+  16: {
+    code: 'KeyW',
+    keyCode: 87,
+    en: 'w',
+    enShift: 'W',
+    ru: 'ц',
+    ruShift: 'Ц',
+  },
+  17: {
+    code: 'KeyE',
+    keyCode: 69,
+    en: 'e',
+    enShift: 'E',
+    ru: 'у',
+    ruShift: 'У',
+  },
+  18: {
+    code: 'KeyR',
+    keyCode: 82,
+    en: 'r',
+    enShift: 'R',
+    ru: 'к',
+    ruShift: 'К',
+  },
+  19: {
+    code: 'KeyT',
+    keyCode: 84,
+    en: 't',
+    enShift: 'T',
+    ru: 'е',
+    ruShift: 'Е',
+  },
+  20: {
+    code: 'KeyY',
+    keyCode: 89,
+    en: 'y',
+    enShift: 'Y',
+    ru: 'н',
+    ruShift: 'Н',
+  },
+  21: {
+    code: 'KeyU',
+    keyCode: 85,
+    en: 'u',
+    enShift: 'U',
+    ru: 'г',
+    ruShift: 'Г',
+  },
+  22: {
+    code: 'KeyI',
+    keyCode: 73,
+    en: 'i',
+    enShift: 'I',
+    ru: 'ш',
+    ruShift: 'Ш',
+  },
+  23: {
+    code: 'KeyO',
+    keyCode: 79,
+    en: 'o',
+    enShift: 'O',
+    ru: 'щ',
+    ruShift: 'Щ',
+  },
+  24: {
+    code: 'KeyP',
+    keyCode: 80,
+    en: 'p',
+    enShift: 'P',
+    ru: 'з',
+    ruShift: 'З',
+  },
+  25: {
+    code: 'BracketLeft',
+    keyCode: 219,
+    en: '[',
+    enShift: '{',
+    ru: 'х',
+    ruShift: 'Х',
+  },
+  26: {
+    code: 'BracketRight',
+    keyCode: 221,
+    en: ']',
+    enShift: '}',
+    ru: 'ъ',
+    ruShift: 'Ъ',
+  },
+  27: {
+    code: 'Backslash',
+    keyCode: 220,
+    en: '\\',
+    enShift: '|',
+    ru: '\\',
+    ruShift: '/',
+  },
+  28: {
+    code: 'CapsLock',
+    keyCode: 20,
+    en: 'CapsLock',
+    enShift: 'CapsLock',
+    ru: 'CapsLock',
+    ruShift: 'CapsLock',
+  },
+  29: {
+    code: 'KeyA',
+    keyCode: 65,
+    en: 'a',
+    enShift: 'A',
+    ru: 'ф',
+    ruShift: 'Ф',
+  },
+  30: {
+    code: 'KeyS',
+    keyCode: 83,
+    en: 's',
+    enShift: 'S',
+    ru: 'ы',
+    ruShift: 'Ы',
+  },
+  31: {
+    code: 'KeyD',
+    keyCode: 68,
+    en: 'd',
+    enShift: 'D',
+    ru: 'в',
+    ruShift: 'В',
+  },
+  32: {
+    code: 'KeyF',
+    keyCode: 70,
+    en: 'f',
+    enShift: 'F',
+    ru: 'а',
+    ruShift: 'А',
+  },
+  33: {
+    code: 'KeyG',
+    keyCode: 71,
+    en: 'g',
+    enShift: 'G',
+    ru: 'п',
+    ruShift: 'П',
+  },
+  34: {
+    code: 'KeyH',
+    keyCode: 72,
+    en: 'h',
+    enShift: 'H',
+    ru: 'р',
+    ruShift: 'Р',
+  },
+  35: {
+    code: 'KeyJ',
+    keyCode: 74,
+    en: 'j',
+    enShift: 'J',
+    ru: 'о',
+    ruShift: 'О',
+  },
+  36: {
+    code: 'KeyK',
+    keyCode: 75,
+    en: 'k',
+    enShift: 'K',
+    ru: 'л',
+    ruShift: 'Л',
+  },
+  37: {
+    code: 'KeyL',
+    keyCode: 76,
+    en: 'l',
+    enShift: 'L',
+    ru: 'д',
+    ruShift: 'Д',
+  },
+  38: {
+    code: 'Semicolon',
+    keyCode: 186,
+    en: ';',
+    enShift: ':',
+    ru: 'ж',
+    ruShift: 'Ж',
+  },
+  39: {
+    code: 'Quote',
+    keyCode: 222,
+    en: "'",
+    enShift: '"',
+    ru: 'э',
+    ruShift: 'Э',
+  },
+  40: {
+    code: 'Enter',
+    keyCode: 13,
+    en: 'Enter',
+    enShift: 'Enter',
+    ru: 'Enter',
+    ruShift: 'Enter',
+  },
+  41: {
+    code: 'ShiftLeft',
+    keyCode: 16,
+    en: 'Shift',
+    enShift: 'Shift',
+    ru: 'Shift',
+    ruShift: 'Shift',
+  },
+  42: {
+    code: 'KeyZ',
+    keyCode: 90,
+    en: 'z',
+    enShift: 'Z',
+    ru: 'я',
+    ruShift: 'Я',
+  },
+  43: {
+    code: 'KeyX',
+    keyCode: 88,
+    en: 'x',
+    enShift: 'X',
+    ru: 'ч',
+    ruShift: 'Ч',
+  },
+  44: {
+    code: 'KeyC',
+    keyCode: 67,
+    en: 'c',
+    enShift: 'C',
+    ru: 'с',
+    ruShift: 'С',
+  },
+  45: {
+    code: 'KeyV',
+    keyCode: 86,
+    en: 'v',
+    enShift: 'V',
+    ru: 'м',
+    ruShift: 'М',
+  },
+  46: {
+    code: 'KeyB',
+    keyCode: 66,
+    en: 'b',
+    enShift: 'B',
+    ru: 'и',
+    ruShift: 'И',
+  },
+  47: {
+    code: 'KeyN',
+    keyCode: 78,
+    en: 'n',
+    enShift: 'N',
+    ru: 'т',
+    ruShift: 'Т',
+  },
+  48: {
+    code: 'KeyM',
+    keyCode: 77,
+    en: 'm',
+    enShift: 'M',
+    ru: 'ь',
+    ruShift: 'Ь',
+  },
+  49: {
+    code: 'Comma',
+    keyCode: 188,
+    en: ',',
+    enShift: '﹤',
+    ru: 'б',
+    ruShift: 'Б',
+  },
+  50: {
+    code: 'Period',
+    keyCode: 190,
+    en: '.',
+    enShift: '﹥',
+    ru: 'ю',
+    ruShift: 'Ю',
+  },
+  51: {
+    code: 'Slash',
+    keyCode: 191,
+    en: '/',
+    enShift: '?',
+    ru: '.',
+    ruShift: ',',
+  },
+  52: {
+    code: 'ShiftRight',
+    keyCode: 16,
+    en: 'Shift',
+    enShift: 'Shift',
+    ru: 'Shift',
+    ruShift: 'Shift',
+  },
+  53: {
+    code: '🌐',
+    keyCode: 0,
+    en: '🌐',
+    enShift: '🌐',
+    ru: '🌐',
+    ruShift: '🌐',
+  },
+  54: {
+    code: 'ControlLeft',
+    keyCode: 17,
+    en: 'Control',
+    enShift: 'Control',
+    ru: 'Control',
+    ruShift: 'Control',
+  },
+  55: {
+    code: 'AltLeft',
+    keyCode: 18,
+    en: 'Option',
+    enShift: 'Option',
+    ru: 'Option',
+    ruShift: 'Option',
+  },
+  56: {
+    code: 'MetaLeft',
+    keyCode: 91,
+    en: 'Command',
+    enShift: 'Command',
+    ru: 'Command',
+    ruShift: 'Command',
+  },
+  57: {
+    code: 'Space',
+    keyCode: 32,
+    en: 'Space',
+    enShift: 'Space',
+    ru: 'Space',
+    ruShift: 'Space',
+  },
+  58: {
+    code: 'MetaRight',
+    keyCode: 93,
+    en: 'Command',
+    enShift: 'Command',
+    ru: 'Command',
+    ruShift: 'Command',
+  },
+  59: {
+    code: 'AltRight',
+    keyCode: 18,
+    en: 'Option',
+    enShift: 'Option',
+    ru: 'Option',
+    ruShift: 'Option',
+  },
+  60: {
+    code: 'ArrowLeft',
+    keyCode: 37,
+    en: '←',
+    enShift: '←',
+    ru: '←',
+    ruShift: '←',
+  },
+  61: {
+    code: 'ArrowUp',
+    keyCode: 38,
+    en: '↑',
+    enShift: '↑',
+    ru: '↑',
+    ruShift: '↑',
+  },
+  62: {
+    code: 'ArrowDown',
+    keyCode: 40,
+    en: '↓',
+    enShift: '↓',
+    ru: '↓',
+    ruShift: '↓',
+  },
+  63: {
+    code: 'ArrowRight',
+    keyCode: 39,
+    en: '→',
+    enShift: '→',
+    ru: '→',
+    ruShift: '→',
+  },
+};
+
+// if pre-saved in localstore - get language and capslock state and make right symbol choise
+let language = localStorage.getItem('language') ? localStorage.getItem('language') : 'en';
+let capslock = localStorage.getItem('capslock') ? localStorage.getItem('capslock') : 'off';
+let isShiftLeftPressed;
+let isShiftRightPressed;
+let keyLangAndRegister;
+function getKeyLangAndRegister() {
+  let isShiftPressed = 'off';
+  if (isShiftLeftPressed === 'on' || isShiftRightPressed === 'on') {
+    isShiftPressed = 'on';
+  }
+  if (language === 'en' && capslock === 'off') {
+    if (isShiftPressed === 'on') {
+      keyLangAndRegister = 'enShift';
+    } else {
+      keyLangAndRegister = 'en';
+    }
+  } else if (language === 'en' && capslock === 'on') {
+    keyLangAndRegister = 'enShift';
+  } else if (language === 'ru' && capslock === 'off') {
+    if (isShiftPressed === 'on') {
+      keyLangAndRegister = 'ruShift';
+    } else {
+      keyLangAndRegister = 'ru';
+    }
+  } else if (language === 'ru' && capslock === 'on') {
+    keyLangAndRegister = 'ruShift';
+  }
+}
+
+const title = document.createElement('h1');
+title.innerHTML = 'Vitrual keyboard';
+
+const description = document.createElement('h3');
+description.innerHTML += 'Hi! Virtual keyboard is based on Mac physical keyboard.<br>';
+description.innerHTML += 'Language change between En and Ru made with GLOBE 🌐&nbsp&nbspkey in left bottom corner.<br>';
+description.innerHTML += 'To make comfort input of special symbols with mouse - this feature implemented through CapsLock key. It is not violation of tasks rules.<br>';
+
+const comments = document.createElement('h3');
+comments.innerHTML += 'If you have any comments or concerns please contact me first.<br>';
+comments.innerHTML += '<br>';
+comments.innerHTML += 'Short self-review:<br>';
+comments.innerHTML += ' -- body is epmty in html +20<br>';
+comments.innerHTML += ' -- pressed keys on physical keyboard are highlighted on virual keyboard +10<br>';
+comments.innerHTML += ' -- language change implemented with special Globe key; language stays after page reload +15<br>';
+comments.innerHTML += ' -- mouse clicked or physically pressed keys inputs symbols in textarea +15<br>';
+comments.innerHTML += ' -- implemented animation on keypress +15<br>';
+comments.innerHTML += ' -- es6+ are used (arrow functions, let/const) +15<br>';
+comments.innerHTML += ' -- eslint with airbnb config used +10<br>';
+comments.innerHTML += ' -- private repo, source in develop branch, pull request and so on +10<br>';
+comments.innerHTML += 'Total: 110<br>';
+
+const textarea = document.createElement('textarea');
+textarea.setAttribute('placeholder', 'Type some text');
+
+const container = document.createElement('div');
+container.classList.add('container');
+document.querySelector('body').append(title, description, textarea, container, comments);
+
+function initVirtualKeyboard() {
+  getKeyLangAndRegister();
+  let output = '';
+  for (let index = 0; index < Object.keys(keyboard).length; index += 1) {
+    // making line-breaks for rows
+    if (index === 14 || index === 28 || index === 41 || index === 53) {
+      output += '<div class="line-break"></div>';
+    }
+    // make container for half-height arrow keys
+    if (index === 61) {
+      output += '<div class="arrow-container">';
+    }
+    // MAIN function
+    output += `<div class="key ${keyboard[index].code.toUpperCase()}`;
+    // opening div to make smaller font-size for special keys
+    if (keyboard[index].en.length > 2) {
+      output += ' small-font';
+    }
+    output += `" data="${index}">${keyboard[index][keyLangAndRegister]}</div>`;
+    // closing div to make smaller font-size for special keys
+    if (index === 62) {
+      output += '</div>';
+    }
+  }
+  container.innerHTML = output;
+  // setting capslock in proper state
+  if (capslock === 'on') {
+    document.querySelector('div[class^="key CAPSLOCK"]').classList.add('lightOn');
+  }
+  if (isShiftLeftPressed === 'on') {
+    document.querySelector('div[class^="key SHIFTLEFT"]').classList.add('special-active');
+  }
+  if (isShiftRightPressed === 'on') {
+    document.querySelector('div[class^="key SHIFTRIGHT"]').classList.add('special-active');
+  }
+  textarea.focus();
+}
+initVirtualKeyboard();
+
+// because chrome  throw error
+// if no mouse click on page was made before physical keyboard interaction was made,
+// sound was disabled due to Student1 vulnerability
+function playKeyPressSound() {
+  // const keySound = new Audio('keydown.wav');
+  // keySound.play();
+  // keySound.currentTime = 0;
+}
+
+function removeClassActive() {
+  document.querySelectorAll('.key.active').forEach((el) => {
+    el.classList.remove('active');
+  });
+}
+
+function backspaceRemoveLastChar() {
+  const textArr = textarea.value.split('');
+  textArr.pop();
+  textarea.value = textArr.join('');
+}
+
+// on real keyboard keypress
+document.onkeydown = (event) => {
+  textarea.focus();
+  playKeyPressSound();
+
+  if (event.code === 'CapsLock') {
+    // CapsLock implementation
+    event.preventDefault();
+    capslock = capslock === 'on' ? 'off' : 'on';
+    localStorage.setItem('capslock', capslock);
+    // setTimeout(removeClassActive(), 150);
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.add('special-active');
+    setTimeout(() => {
+      initVirtualKeyboard();
+    }, 150);
+  } else if (event.code === 'ShiftLeft') {
+    // shift left
+    isShiftLeftPressed = 'on';
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.add('special-active');
+    initVirtualKeyboard();
+  } else if (event.code === 'ShiftRight') {
+    // shift right
+    isShiftRightPressed = 'on';
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.add('special-active');
+    initVirtualKeyboard();
+  } else if (
+    // special keys
+    event.code === 'MetaLeft'
+    || event.code === 'MetaRight'
+    || event.code === 'AltLeft'
+    || event.code === 'AltRight'
+    || event.code === 'ControlLeft'
+    || event.code === 'CapsLock'
+  ) {
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.add('special-active');
+  } else {
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.add('active');
+    setTimeout(removeClassActive, 150);
+  }
+  // tab key
+  if (event.code === 'Tab') {
+    event.preventDefault();
+    textarea.value += '\t';
+    event.target.classList.add('active');
+  }
+  // arrows
+  if (event.code === 'ArrowLeft' || event.code === 'ArrowUp' || event.code === 'ArrowDown' || event.code === 'ArrowRight') {
+    event.preventDefault();
+    switch (event.code) {
+      case 'ArrowLeft':
+        textarea.value += '←';
+        break;
+      case 'ArrowUp':
+        textarea.value += '↑';
+        break;
+      case 'ArrowDown':
+        textarea.value += '↓';
+        break;
+      case 'ArrowRight':
+        textarea.value += '→';
+        break;
+      default:
+        event.preventDefault();
+        break;
+    }
+  }
+};
+
+document.onkeyup = (event) => {
+  if (event.code === 'CapsLock') {
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.remove('special-active');
+  }
+  if (event.code === 'ShiftLeft' && isShiftLeftPressed === 'on') {
+    isShiftLeftPressed = 'off';
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.remove('special-active');
+    initVirtualKeyboard();
+  }
+  if (event.code === 'ShiftRight' && isShiftRightPressed === 'on') {
+    isShiftRightPressed = 'off';
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.remove('special-active');
+    initVirtualKeyboard();
+  }
+  if (
+    event.code === 'MetaLeft'
+    || event.code === 'MetaRight'
+    || event.code === 'AltLeft'
+    || event.code === 'AltRight'
+    || event.code === 'ControlLeft'
+  ) {
+    document.querySelector(`div[class^="key ${event.code.toUpperCase()}"]`).classList.remove('special-active');
+  }
+};
+
+// on mouse click on virtual keyboard
+document.querySelector('.container').addEventListener('click', (el) => {
+  textarea.focus();
+  if (el.target.classList.contains('container')) {
+    el.preventDefault();
+  } else {
+    playKeyPressSound();
+
+    if (el.target.classList.contains('🌐')) {
+      // change language
+      language = language === 'ru' ? 'en' : 'ru';
+      localStorage.setItem('language', language);
+      el.target.classList.add('special-active');
+      setTimeout(() => {
+        el.target.classList.remove('special-active');
+        initVirtualKeyboard();
+      }, 150);
+    } else if (el.target.classList.contains('CAPSLOCK')) {
+      // CapsLock implementation
+      capslock = capslock === 'on' ? 'off' : 'on';
+      localStorage.setItem('capslock', capslock);
+      el.target.classList.add('special-active');
+      setTimeout(() => {
+        el.target.classList.remove('special-active');
+        initVirtualKeyboard();
+      }, 150);
+    } else if (
+      el.target.classList.contains('SHIFTLEFT')
+      || el.target.classList.contains('SHIFTRIGHT')
+      || el.target.classList.contains('METALEFT')
+      || el.target.classList.contains('METARIGHT')
+      || el.target.classList.contains('ALTLEFT')
+      || el.target.classList.contains('ALTRIGHT')
+      || el.target.classList.contains('CONTROLLEFT')
+    ) {
+      el.target.classList.add('special-active');
+      setTimeout(() => {
+        el.target.classList.remove('special-active');
+      }, 150);
+    } else if (el.target.classList.contains('TAB')) {
+      textarea.value += '\t';
+      el.target.classList.add('active');
+      setTimeout(removeClassActive, 150);
+    } else if (el.target.classList.contains('BACKSPACE')) {
+      backspaceRemoveLastChar();
+      el.target.classList.add('active');
+      setTimeout(removeClassActive, 150);
+    } else if (el.target.classList.contains('SPACE')) {
+      el.target.classList.add('active');
+      textarea.value += ' ';
+      setTimeout(removeClassActive, 150);
+    } else if (el.target.classList.contains('ENTER')) {
+      el.target.classList.add('active');
+      textarea.value += '\n';
+      setTimeout(removeClassActive, 150);
+    } else {
+      // MAIN function
+      textarea.value += el.target.innerHTML;
+      el.target.classList.add('active');
+      setTimeout(removeClassActive, 150);
+    }
+  }
+});
